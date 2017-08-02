f1591df1efbe66a0b9761c60961e25a98c8da8b7
lucio-li <lq@lq555.cn>
2017-08-02 11:25:08 +0800

    初始化

---
+++ src/main/java/com/maven/MavenTest/App.java
@@ -0,0 +1,20 @@
+package com.maven.MavenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public  int max(int a, int b) {
+		
+		if (a > b) {
+			System.out.println(a);
+		} else {
+			System.out.println(b);
+		}
+		return 1;
+	}
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}

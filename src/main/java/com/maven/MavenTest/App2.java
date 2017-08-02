233299e4fff48645cdfc4974b33bd01f9d44e3f1
lucio-li <lq@lq555.cn>
2017-08-02 14:59:16 +0800

    初始化

---
+++ src/main/java/com/maven/MavenTest/App2.java
@@ -0,0 +1,22 @@
+package com.maven.MavenTest;
+
+public class App2 {
+	public  int max(int a, int b) {
+		
+		if (a > b) {
+			System.out.println(a);
+		} else {
+			System.out.println(b);
+		}
+		return 1;
+	}
+	public  int min(int a, int b) {
+		
+		if (a > b) {
+			System.out.println(b);
+		} else {
+			System.out.println(a);
+		}
+		return 1;
+	}
+}

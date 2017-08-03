e556f9a88a45830a8dd2c881493175b976f2e4ba
lucio-li <lq@lq555.cn>
2017-08-03 14:41:45 +0800

    加新的测试类

---
+++ src/main/java/com/maven/MavenTest/NewTestT.java
@@ -0,0 +1,11 @@
+package com.maven.MavenTest;
+
+import junit.framework.TestCase;
+
+public class NewTestT extends TestCase{
+	 public void testApp4() {
+       assertTrue( true );
+       App2 app2 = new App2();
+       app2.max(5, 4);
+   }
+}

22285309970e4f7775a8c47bdc3aefac52de97b9
lucio-li <lq@lq555.cn>
2017-08-03 15:09:47 +0800

    增加测试类

---
+++ src/test/java/com/maven/MavenTest/APPee.java
@@ -0,0 +1,11 @@
+package com.maven.MavenTest;
+
+import junit.framework.TestCase;
+
+public class APPee extends TestCase{
+	public void testApp5() {
+        assertTrue( true );
+        App2 app2 = new App2();
+        app2.max(5, 4);
+    }
+}

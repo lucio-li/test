dbdc01b44edcd8cf8c08d926786d8062c5295d2d
lucio-li <592782252@qq.com>
2017-08-09 16:22:14 +0800

    修改junit为3.8版本

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,42 +3,60 @@
 import com.jenkins.App;
 import com.jenkins.App2;
 
+
 import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
-   
+public class AppTest extends TestCase{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName ) {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
     /**
      * Rigourous Test :-)
      */
-	@org.junit.Test
-    public void testApp() {
-        App app = new App();
-        app.max(5, 4);
-    }
-	@org.junit.Test
-    public void testApp2() {
-        App2 app2 = new App2();
-        app2.max(5, 4);
-    }
-	@org.junit.Test
-    public void testApp3() {
-        App2 app2 = new App2();
-        app2.max(5, 4);
-    }
-	@org.junit.Test
-    public void testApp4() {
-        App2 app2 = new App2();
-        app2.max(5, 4);
-        app2.max(4, 5);
-        app2.min(4, 5);
-        app2.max(5, 4);
-    }
-	@org.junit.Test
+	    public void testApp() {
+	        assertTrue( true );
+	        App app = new App();
+	        app.max(5, 4);
+	    }
+	    public void testApp2() {
+	        assertTrue( true );
+	        App2 app2 = new App2();
+	        app2.max(5, 4);
+	    }
+	    public void testApp3() {
+	        assertTrue( true );
+	        App2 app2 = new App2();
+	        app2.max(5, 4);
+	    }
+	    public void testApp4() {
+	        assertTrue( true );
+	        App2 app2 = new App2();
+	        app2.max(5, 4);
+	        app2.max(4, 5);
+	        app2.min(4, 5);
+	        app2.max(5, 4);
+	    }
     public void testApp5() {
-       // assertTrue( true );
+        assertTrue( true );
         App2 app2 = new App2();
         app2.max(5, 4);
         app2.max(4, 5);
